251f2b865b2ffcd3296512cca533ba3cf90760fb
Johan Siebens <johan.siebens@gmail.com>
2022-11-08 08:33:16 +0100

    client: move expirySeconds to correct place and add options to configure create key request
    Signed-off-by: Johan Siebens <johan.siebens@gmail.com>

--- tailscale/client_test.go
+++ tailscale/client_test.go
@@ -623,7 +623,6 @@ func TestClient_CreateKey(t *testing.T) {
 	capabilities.Devices.Create.Reusable = true
 	capabilities.Devices.Create.Preauthorized = true
 	capabilities.Devices.Create.Tags = []string{"test:test"}
-	capabilities.Devices.Create.Expiry = 3600
 
 	expected := tailscale.Key{
 		ID:           "test",
@@ -640,6 +639,45 @@ func TestClient_CreateKey(t *testing.T) {
 	assert.EqualValues(t, expected, actual)
 	assert.Equal(t, http.MethodPost, server.Method)
 	assert.Equal(t, "/api/v2/tailnet/example.com/keys", server.Path)
+
+	var actualReq tailscale.CreateKeyRequest
+	assert.NoError(t, json.Unmarshal(server.Body.Bytes(), &actualReq))
+	assert.EqualValues(t, capabilities, actualReq.Capabilities)
+	assert.EqualValues(t, 0, actualReq.ExpirySeconds)
+}
+
+func TestClient_CreateKeyWithExpirySeconds(t *testing.T) {
+	t.Parallel()
+
+	client, server := NewTestHarness(t)
+	server.ResponseCode = http.StatusOK
+
+	capabilities := tailscale.KeyCapabilities{}
+	capabilities.Devices.Create.Ephemeral = true
+	capabilities.Devices.Create.Reusable = true
+	capabilities.Devices.Create.Preauthorized = true
+	capabilities.Devices.Create.Tags = []string{"test:test"}
+
+	expected := tailscale.Key{
+		ID:           "test",
+		Key:          "thisisatestkey",
+		Created:      time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+		Expires:      time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC),
+		Capabilities: capabilities,
+	}
+
+	server.ResponseBody = expected
+
+	actual, err := client.CreateKey(context.Background(), capabilities, tailscale.WithKeyExpiry(1440*time.Second))
+	assert.NoError(t, err)
+	assert.EqualValues(t, expected, actual)
+	assert.Equal(t, http.MethodPost, server.Method)
+	assert.Equal(t, "/api/v2/tailnet/example.com/keys", server.Path)
+
+	var actualReq tailscale.CreateKeyRequest
+	assert.NoError(t, json.Unmarshal(server.Body.Bytes(), &actualReq))
+	assert.EqualValues(t, capabilities, actualReq.Capabilities)
+	assert.EqualValues(t, 1440, actualReq.ExpirySeconds)
 }
 
 func TestClient_GetKey(t *testing.T) {
